3171142141002eda74c3b80b056d92a3abc5c2f0
Dale Terrell <tragicid@gmail.com>
2021-02-20 08:28:58 -0600

    add cors mapping

--- backend/src/main/gameshelf/GameShelfApplication.java
+++ backend/src/main/gameshelf/GameShelfApplication.java
@@ -1,3 +1,9 @@
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class GameShelfApplication {
-    
+
+    public static void main(String[] args) {
+        SpringApplication.run(GameShelfApplication.class, args);
+    }
 }
--- backend/src/main/gameshelf/WebConfig.java
+++ backend/src/main/gameshelf/WebConfig.java
@@ -1,3 +1,15 @@
-public class WebConfig {
-    
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**");
+
+    }
+
 }
